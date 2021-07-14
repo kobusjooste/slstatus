9d90fb427ece0a2265d1372afd3f2885db8782b8
koos (gracie) <kuberkoos@gmail.com>
2021-07-14 12:25:50 +0200

    add ram, cpu and bat percs

--- config.h
+++ config.h
@@ -65,5 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ ram_perc,     "mem:%s%% | ", NULL },
+	{ cpu_perc,     "cpu:%s%% | ", NULL },
+	{ battery_perc, "bat:%s%% | ", "BAT0" },
+	{ datetime,     "%s",          "%F %T" },
 };
